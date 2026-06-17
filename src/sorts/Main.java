93443b644c994aacb8162bfd26d95fcfdf70cea9
Nagendra <nagendra.burusu@thoughtclan.com>
2026-06-17 19:49:42 +0530

    Added code for the selection sorting

--- src/sorts/Main.java
+++ src/sorts/Main.java
@@ -3,14 +3,15 @@ package sorts;
 public class Main {
 	
 	public static void main(String[] args) {
-		BubbleSort bubbleSort = new BubbleSort();
+//		BubbleSort sort = new BubbleSort();
+		SelectionSort sort = new SelectionSort();
 		Integer[] input = {5, 3, 8, 1, 2};
 		for (int i = 0; i < input.length; i++) {
 			System.out.print(input[i] +" ");
 		}
 		System.out.println();
 		System.out.println("After sorting the array");
-		bubbleSort.sort(input);
+		sort.sort(input);
 		for (int i = 0; i < input.length; i++) {
 			System.out.print(input[i] +" ");
 		}
--- src/sorts/SortUtils.java
+++ src/sorts/SortUtils.java
@@ -6,10 +6,26 @@ final class SortUtils {
 		
 	}
 
+	/**
+     * Compares two elements to see if the first is greater than the second.
+     *
+     * @param firstElement  the first element to compare
+     * @param secondElement the second element to compare
+     * @return true if the first element is greater than the second, false otherwise
+     */
 	public static <T extends Comparable<T>> boolean greater(T firstElement, T secondElement) {
 		return firstElement.compareTo(secondElement) > 0;
 	}
 
+
+	/**
+     * Swaps two elements at the given positions in an array.
+     *
+     * @param array the array in which to swap elements
+     * @param i     the index of the first element to swap
+     * @param j     the index of the second element to swap
+     * @param <T>   the type of elements in the array
+     */
 	public static <T> void swap(T[] array, int i, int j) {
 		if(i!=j) {
 			final T temp = array[i];
@@ -17,5 +33,17 @@ final class SortUtils {
 			array[j] = temp;
 		}
 		
+	}
+
+	/**
+     * Compares two elements to see if the first is less than the second.
+     *
+     * @param firstElement  the first element to compare
+     * @param secondElement the second element to compare
+     * @return true if the first element is less than the second, false otherwise
+     */
+	public static <T extends Comparable<T>> boolean less(T firstElement, T secondElement) {
+		// TODO Auto-generated method stub
+		return firstElement.compareTo(secondElement) < 0;
 	}
 }
